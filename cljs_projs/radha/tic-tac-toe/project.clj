9ec68c2b023913dac6d66e4d10ac0cf4958c0960
ratedrworld <radhakrishna.kamath@ves.ac.in>
2017-07-26 18:31:15 +0530

    rated: code of x-zero

---
+++ cljs_projs/radha/tic-tac-toe/project.clj
@@ -0,0 +1,27 @@
+(defproject tic-tac-toe "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.9.671"]]
+  :plugins [[lein-cljsbuild "1.1.2"]]
+  :cljsbuild {:builds
+              [{:id "dev"
+                :source-paths ["src/cljs"]
+                :compiler {:main tic-tac-toe.core
+                           :asset-path "js/app"
+                           :output-to "resources/public/js/app/app.js"
+                           :output-dir "resources/public/js/app"
+                           :optimizations :none
+                           :pretty-print true}}
+               {:id "min"
+                :source-paths ["src/cljs"]
+                :compiler {:main tic-tac-toe.core
+                           :asset-path "js/app"
+                           :output-to "resources/public/js/app/app.js"
+                           :optimizations :whitespace
+                           :pretty-print false}}]}
+  :main ^:skip-aot tic-tac-toe.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
