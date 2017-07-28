cfccd0168d9128bb353425b645adbf243b232522
sam16 <cooldudeyash96@gmail.com>
2017-07-28 12:17:20 +0530

    Modified X/O core

--- cljs_projs/sam16/tic_tac_toe/src/cljs/tic_tac_toe/core.cljs
+++ cljs_projs/sam16/tic_tac_toe/src/cljs/tic_tac_toe/core.cljs
@@ -2,10 +2,35 @@
   (:require [clojure.string :as String]))
 
 (defn get-input []
+  (mapv vec
+        (partition 3
+                   (for [i (range 3)
+                         j (range 3)
+                         :let [blockno (str "box" i j)]]
+                     (.-innerHTML (.getElementById js/document blockno))))))
+
+;; Dont use vec vec Use mapv vec
+#_(defn get-input []
   (vec (for [i (range 3)]
          (vec (for [ j (range 3)
                     :let [blockno (str "box" i j)]]
-              (.-innerHTML (.getElementById js/document blockno)))))))
+                (.-innerHTML (.getElementById js/document blockno)))))))
+
+
+(defn set-output [board]
+  (for [i (range 3)
+        j (range 3)
+        :let [blockno (str "box" i j)]]
+    (set! (.-innerHTML (.getElementById js/document (str "box" i j)))
+          (get-in board [i j]))))
+
+(defn restart []
+  (.log js/console "hello")
+  (doall (for [i (range 3)
+               j (range 3)
+               :let [blockno (str "box" i j)]]
+           (set! (.-innerHTML (.getElementById js/document (str "box" i j)))
+                 ""))))
 
 (defn x-or-o? [input]
   (if (odd? (count (filter empty? (flatten input))))
@@ -17,21 +42,38 @@
          (vec (for [y (range 3)]
                 (if (and (= x i) (= y j))
                   (do
-                    (set! (.-innerHTML (.getElementById js/document (str "box" i j))) (x-or-o? brd))
+                    (set! (.-innerHTML (.getElementById js/document (str "box" i j)))
+                          (x-or-o? brd))
                     (x-or-o? brd))
                   (get-in brd [x y])))))))
 
 (defn done-row? [i board]
-  (reduce (fn [a x] (if (and (= a x) (not= x ""))  a false)) (mapv #(get-in board [i %]) (range 0 3))))
+  (reduce (fn [a x]
+            (if (and (= a x) (not= x ""))
+              a
+              false))
+          (mapv #(get-in board [i %]) (range 0 3))))
 
 (defn done-col? [i board]
-  (reduce (fn [a x] (if (and (= a x) (not= x ""))  a false)) (mapv #(get-in board [% i]) (range 0 3))))
+  (reduce (fn [a x]
+            (if (and (= a x) (not= x ""))
+              a
+              false))
+          (mapv #(get-in board [% i]) (range 0 3))))
 
 (defn diag1? [board]
-  (reduce (fn [a x] (if (and (= a x) (not= x ""))  a false)) (mapv #(get-in board [%1 %2]) (range 0 3) (range 0 3))))
+  (reduce (fn [a x]
+            (if (and (= a x) (not= x ""))
+              a
+              false))
+          (mapv #(get-in board [%1 %2]) (range 0 3) (range 0 3))))
 
 (defn diag2? [board]
-  (reduce (fn [a x] (if (and (= a x) (not= x ""))  a false)) (mapv #(get-in board [%1 %2]) (range 0 3) (reverse (range 0 3)))))
+  (reduce (fn [a x]
+            (if (and (= a x) (not= x ""))
+              a
+              false))
+          (mapv #(get-in board [%1 %2]) (range 0 3) (reverse (range 0 3)))))
 
 (defn won? [board]
   (or (some #(if (not= % false) %) (map #(done-row? % board) (range 3)))
@@ -45,8 +87,8 @@
   (let [inp-board (get-input)
         ;;zk (won? (check))
         new (make-move i j inp-board)]
-    (js/alert new)
     (if (won? new)
-      (js/alert (str "Player " (won? new) " Won..!!"))
-      false)
-    ))
+      (do
+        (js/alert (str "Player " (won? new) " Won..!!"))
+        (restart))
+      false)))
