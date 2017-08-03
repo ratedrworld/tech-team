97601ea183f08c25c248075732bc0b34b77fd2c1
Sachin <sachinharpalani3795@gmail.com>
2017-08-03 18:37:52 +0530

    WORK-IN-PROGRESS Whos game UI + logic

--- cljs_projs/sachin/whos-first-game/src/cljs/whos_first_game/core.cljs
+++ cljs_projs/sachin/whos-first-game/src/cljs/whos_first_game/core.cljs
@@ -7,7 +7,16 @@
 ;; Views
 
 (defn home-page []
-  [:div [:h2 "Welcome to whos-first-game"]])
+  [:div [:h2 "Welcome to whos-first-game"]
+   [:input {:type "button" :value "PLAYER A" :style {:height "40"
+                                                     :width "200"}}]
+   [:input {:type "button" :value "PLAYER B" :style {:height "40"
+                                                     :width "200"}}]
+   (for [i (range 10)]
+     [:div (for [j (range 10)]
+             [:input {:type "button" :style {:background-color "red"
+                                             :height "50"
+                                             :width "50"}}])])])
 
 
 ;; -------------------------
@@ -44,98 +53,3 @@
   []
   (let [size (:size @board)]
     (map #(swap! board assoc-in [:board % :value] %) (range (* size size)))))
-
-
-(defn get-element
-  [i j]
-  (first (filter (fn [x]
-                   (let [values (map val x)]
-                     (and
-                      (= i (first values))
-                      (= j (second values)))))
-                 (:board @board))))
-
-(defn get-position
-  [value]
-  (let [map1  (filter (fn [x]
-                        (= value (:value x)))
-                      (get-in @board [:board]))
-        val-map (flatten (map vals map1))]
-    [(first val-map) (second val-map)]))
-
-
-
-(defn update-current-position
-  [x y]
-  (swap! board assoc-in [:current-pos] [x y]))
-
-
-(defn move-player
-  [steps direction]
-  (let [[x-pos y-pos] (:current-pos @board)]
-    (condp = direction
-      :right (let [ans (mapv (fn [y]
-                               (:value (get-element x-pos y)))
-                             (range y-pos (+ y-pos steps)))
-                   [x y] (get-position (last ans))]
-               (update-current-position (inc x) y)
-               ans)
-
-      :down (let [ans (mapv (fn [x]
-                              (:value (get-element x y-pos)))
-                            (range x-pos (+ x-pos steps)))
-                  [x y] (get-position (last ans))]
-              (update-current-position x (dec  y))
-              ans)
-
-      :left (let [ans (mapv (fn [y]
-                              (:value (get-element x-pos y)))
-                            (reverse  (range (- (inc  y-pos) steps) (inc y-pos))))
-                  [x y] (get-position (last ans))]
-              (update-current-position (dec x) y)
-              ans)
-
-      :up (let [ans (mapv (fn [x]
-                            (:value (get-element x y-pos)))
-                          (reverse (range (- (inc  x-pos) steps) (inc x-pos))))
-                [x y] (get-position (last ans))]
-            (update-current-position x (inc y))
-            ans))))
-
-
-
-(defn change-direction
-  [dir]
-  (swap! board assoc-in [:dir] dir))
-
-
-(defn execute-spiral
- [element]
-  (mapcat (fn [x]
-            (let [ele (element @board)
-                  dir (:dir ele)
-                  cur-pos (:current-pos ele)]
-              (condp = dir
-                :right (do
-                         (change-direction :down)
-                         (move-player x :right))
-
-                :down  (do
-                         (change-direction :left)
-                         (move-player x :down))
-
-                :left (do
-                        (change-direction :up)
-                        (move-player x :left))
-
-                :up (do
-                      (change-direction :right)
-                      (move-player x :up)))))
-          (get-path-dir (:size @board))))
-
-(defn get-path-dir
-  [size]
-  (reverse (drop 1
-                 (drop-last (mapcat
-                             (fn [x y] [x y])
-                             (range 2 (+ 2 size) 2) (range 2 (+ 2 size) 2))))))
