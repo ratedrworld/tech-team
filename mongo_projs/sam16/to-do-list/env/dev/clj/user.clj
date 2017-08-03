194e6b764d4d978d1a0c144c1fef5e02a2ceed24
sam16 <cooldudeyash96@gmail.com>
2017-08-03 10:33:07 +0530

    Created folder Mongo Projs for Sharing MongoDB Code

---
+++ mongo_projs/sam16/to-do-list/env/dev/clj/user.clj
@@ -0,0 +1,15 @@
+(ns user
+  (:require [mount.core :as mount]
+            to-do-list.core))
+
+(defn start []
+  (mount/start-without #'to-do-list.core/repl-server))
+
+(defn stop []
+  (mount/stop-except #'to-do-list.core/repl-server))
+
+(defn restart []
+  (stop)
+  (start))
+
+
